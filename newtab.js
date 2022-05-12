4eb2899f4063bfd27646702b95e697282cfee382
Marius Röhm <marius.roehm@gmail.com>
2022-05-12 18:39:55 +0200

    init

---
+++ newtab.js
@@ -0,0 +1,9 @@
+/*
+Just draw a border round the document.body.
+*/
+document.body.style.border = "5px solid red";
+
+/*
+Open every reference in a new tab.
+*/
+var links = document.getElementsByTagName("a");
