ef90c375a6558cf5430945a4edd67acb353c92ac
Marius Röhm <marius.roehm@gmail.com>
2022-10-25 19:21:38 +0200

    disable animations

--- newtab.js
+++ newtab.js
@@ -5,3 +5,26 @@ var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
   links[i].target = "_blank";
 }
+
+// open Menue
+clickByXPath("/html/body/div[1]/div[11]");
+// disable animations
+clickByXPath("/html/body/div[4]/nav/ol/li[2]");
+// close Menue
+clickByXPath('//*[@id="close"]');
+
+/*
+Click on Element by XPath
+*/
+function clickByXPath(path) {
+  var element = document.evaluate(
+    path,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+  if (element) {
+    element.click();
+  }
+}
